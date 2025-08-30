1ccc4c6aea36fdc6a29ed89b89e79c00c9381ba5
Binoj Madhurama <136594964+binojmadhuranga@users.noreply.github.com>
2025-08-30 12:55:20 +0530

    Update DoctorServlet.java

--- src/main/java/com/example/acpt/demoee/Controller/DoctorServlet.java
+++ src/main/java/com/example/acpt/demoee/Controller/DoctorServlet.java
@@ -18,6 +18,7 @@ import java.util.List;
 @WebServlet(urlPatterns = {"/doctor"})
 public class DoctorServlet extends HttpServlet {
 
+    
     private final DoctorService service = new DoctorServiceIMPL();
     private final Gson gson = new Gson();
 
